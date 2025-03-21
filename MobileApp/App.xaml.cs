fc0cb6d602b9396c42abd9926e59a4e1f9b18193
kasjaduddin <kasjaduddin@outlook.com>
2025-03-21 23:12:44 +0700

    feat: add user preferences

--- MobileApp/App.xaml.cs
+++ MobileApp/App.xaml.cs
@@ -1,12 +1,35 @@
-﻿namespace MobileApp
+﻿using MobileApp.Models;
+using System.Text.Json;
+
+namespace MobileApp
 {
     public partial class App : Application
     {
+        public static User? CurrentUser { get; set; }
+
         public App()
         {
             InitializeComponent();
 
+            LoadData();
+
             MainPage = new MainPage();
+        }
+
+        // Initialize User from JSON file
+        private void LoadData()
+        {
+            if (Preferences.ContainsKey("UserName"))
+            {
+                string userName = Preferences.Get("UserName", "");
+                string userPhoneNumber = Preferences.Get("UserPhoneNumber", "");
+
+                CurrentUser = new User(userName, userPhoneNumber);
+            }
+            else
+            {
+                CurrentUser = new User();
+            }
         }
     }
 }
