13c252960220edc5372c3943915d366529cd3170
kasjaduddin <kasjaduddin@outlook.com>
2025-03-23 17:12:39 +0700

    feat: add load emergency contact data on startup

--- MobileApp/App.xaml.cs
+++ MobileApp/App.xaml.cs
@@ -5,6 +5,7 @@ namespace MobileApp
     public partial class App : Application
     {
         public static User? CurrentUser { get; set; }
+        public static EmergencyContact? EmergencyContact { get; set; }
 
         public App()
         {
@@ -22,18 +23,35 @@ namespace MobileApp
             {
                 string userName = Preferences.Get("username", "");
                 string userPhoneNumber = Preferences.Get("user_phone_number", "");
-                
+
                 LoadUserData(userName, userPhoneNumber);
             }
             else
             {
                 CurrentUser = new User();
             }
+
+            if (Preferences.ContainsKey("contact_name"))
+            {
+                string contactName = Preferences.Get("contact_name", "");
+                string contactPhoneNumber = Preferences.Get("contact_phone_number", "");
+
+                LoadContactsData(contactName, contactPhoneNumber);
+            }
+            else
+            {
+                EmergencyContact = new EmergencyContact();
+            }
         }
 
         public static void LoadUserData(string userName, string userPhoneNumber)
         {
             CurrentUser = new User(userName, userPhoneNumber);
+        }
+
+        public static void LoadContactsData(string contactName, string contactPhoneNumber)
+        {
+            EmergencyContact = new EmergencyContact(contactName, contactPhoneNumber);
         }
     }
 }
