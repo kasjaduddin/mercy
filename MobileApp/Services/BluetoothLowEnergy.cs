883d34a7909ef41b271ad1c0eec40e65dbed372f
kasjaduddin <kasjaduddin@outlook.com>
2025-04-02 22:15:16 +0700

    feat: add connecting to Bluetooth Low Energy devices

--- MobileApp/Services/BluetoothLowEnergy.cs
+++ MobileApp/Services/BluetoothLowEnergy.cs
@@ -1,18 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
 using System.Threading.Tasks;
+#if ANDROID
+using Android.Content;
+#endif
 
 namespace MobileApp.Services
 {
     public class BluetoothLowEnergy
     {
-    #if ANDROID
+        private static BluetoothState state;
+        private static IBluetoothLE ble = CrossBluetoothLE.Current;
+        private static IAdapter adapter = CrossBluetoothLE.Current.Adapter;
+        private static List<IDevice> deviceList = new List<IDevice>();
+
+        public static BluetoothState State
+        {
+            get => state;
+        }
+
+#if ANDROID
         public static async Task<PermissionStatus> CheckAndRequestBluetoothPermission()
         {
             PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.Bluetooth>();
-            Console.WriteLine("Bluetooth permission status: " + status);
+
             if (status == PermissionStatus.Granted)
                 return status;
 
@@ -22,9 +33,66 @@ namespace MobileApp.Services
             }
 
             status = await Permissions.RequestAsync<Permissions.Bluetooth>();
-            Console.WriteLine("Bluetooth permission status: " + status);
+
             return status;
         }
-    #endif
+
+        private static async Task GetBluetoothStatus()
+        {
+            PermissionStatus status = await CheckAndRequestBluetoothPermission();
+            
+            if (status == PermissionStatus.Granted)
+            {
+                state = ble.State;
+                
+                ble.StateChanged += async (s, e) =>
+                {
+                    Console.WriteLine($"The bluetooth state changed to {e.NewState}");
+                    await ConnectToBluetoothDevices();
+                };
+            }
+        }
+
+        private static void OpenBluetoothSettings()
+        {
+            if (state == BluetoothState.On && !CheckConnectedDevices())
+            {
+                try
+                {
+                    var intent = new Intent(Android.Provider.Settings.ActionBluetoothSettings);
+                    intent.AddFlags(ActivityFlags.NewTask);
+                    Android.App.Application.Context.StartActivity(intent);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error opening Bluetooth settings: {ex.Message}");
+                }
+            }
+        }
+
+        private static bool CheckConnectedDevices()
+        {
+            adapter.DeviceConnected += (s, a) =>
+            {
+                deviceList.Add(a.Device);
+            };
+
+            foreach (var device in deviceList)
+            {
+                if (device.Name.Contains("Mercy"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static async Task ConnectToBluetoothDevices()
+        {
+            await GetBluetoothStatus();
+            OpenBluetoothSettings();
+        }
+#endif
     }
 }
