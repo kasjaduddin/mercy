9881f576518ac3dd9e4c2740f2dfd3e7a3b65d4b
kasjaduddin <kasjaduddin@outlook.com>
2025-04-09 15:57:40 +0700

    feat: add heart condition monitor model

---
+++ MobileApp/Models/HeartConditionMonitor.cs
@@ -0,0 +1,18 @@
+﻿namespace MobileApp.Models
+{
+    public class HeartConditionMonitor
+    {
+        private readonly Queue<ushort> ecgQueue = new Queue<ushort>();
+        private readonly object lockObj = new object();
+
+        public void AddData(ushort data)
+        {
+            lock (lockObj)
+            {
+                ecgQueue.Enqueue(data);
+                if (ecgQueue.Count > 100)
+                    ecgQueue.Dequeue();
+            }
+        }
+    }
+}
