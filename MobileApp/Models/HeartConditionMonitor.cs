33fb7c52f4868fd2dc1f2e448b2f5b8abab35e4b
kasjaduddin <kasjaduddin@outlook.com>
2025-04-10 21:34:26 +0700

    feat: add ecg visualization

--- MobileApp/Models/HeartConditionMonitor.cs
+++ MobileApp/Models/HeartConditionMonitor.cs
@@ -2,15 +2,27 @@
 {
     public class HeartConditionMonitor
     {
-        private readonly Queue<ushort> ecgQueue = new Queue<ushort>();
-        private readonly object lockObj = new object();
+        private readonly Queue<ushort> ecgQueue = new();
+        private readonly object lockObj = new();
+
+        public List<ECGDataPoint> GetECGChartData()
+        {
+            lock (lockObj)
+            {
+                return ecgQueue.Select((value, index) => new ECGDataPoint
+                {
+                    Time = index,
+                    Value = value
+                }).ToList();
+            }
+        }
 
         public void AddData(ushort data)
         {
             lock (lockObj)
             {
                 ecgQueue.Enqueue(data);
-                if (ecgQueue.Count > 100)
+                if (ecgQueue.Count > 180)
                     ecgQueue.Dequeue();
             }
         }
